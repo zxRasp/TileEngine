fe6a50680ec00d74144213372c7777ada044b98c
zxRasp <zxrasp@gmail.com>
2013-02-01 12:21:12 +0200

    Implement rect intersecr

--- src/util/Collisions.cpp
+++ src/util/Collisions.cpp
@@ -2,6 +2,13 @@
 
 bool isRectIntersect(RECT rc1, RECT rc2)
 {
-    // TODO: write collisions check
-    return true;
+    int rc1right  = rc1.x + rc1.w;
+    int rc1bottom = rc1.y + rc1.h;
+    int rc2right  = rc2.x + rc2.w;
+    int rc2bottom = rc2.y + rc2.h;
+
+    return  rc1.x < rc2right  &&
+            rc1right > rc2.x  &&
+            rc1.y < rc2bottom &&
+            rc1bottom > rc2.y;
 }
